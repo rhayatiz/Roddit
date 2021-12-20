4a867ceab12db255c5f9841ed05877c9880e7eb6
mayol <mayolboucaud@gmail.com>
2021-12-20 14:30:33 +0100

    v1.2.1 amélioration code like

--- js/script.js
+++ js/script.js
@@ -9,32 +9,7 @@ function actionLikePost(idUser, idPost, statut)
         success:function(data) {
             if(data)
             {
-                var btnLike = document.querySelector('#btnLike_' + idPost);
-                var btnDislike = document.querySelector('#btnDislike_' + idPost);
-                console.log(data.data)
-                if(data.data == '2')
-                {
-                    if (btnDislike.classList.contains('red')) {
-                        btnDislike.classList.remove('red');
-                    }
-                    btnLike.classList.add('green');
-                }
-                else if(data.data == '1')
-                {
-                    if (btnLike.classList.contains('green')) {
-                        btnLike.classList.remove('green');
-                    }
-                    btnDislike.classList.add('red');
-                }
-                else if(data.data == '0')
-                {
-                    if (btnLike.classList.contains('green')) {
-                        btnLike.classList.remove('green');
-                    }
-                    if (btnDislike.classList.contains('red')) {
-                        btnDislike.classList.remove('red');
-                    }
-                }
+                changebtnLike(idPost, data.data);
             }
         },
     });
@@ -49,62 +24,38 @@ function getAllLikePostByUser(idUser, statut)
         success:function(data) {
 
             data.forEach(function(d) {
-                // code
-                console.log(d);
-                var btnLike = document.querySelector('#btnLike_' + d.idPost);
-                var btnDislike = document.querySelector('#btnDislike_' + d.idPost);
-
-                if(d.statut == '2')
-                {
-                    if (btnDislike.classList.contains('red')) {
-                        btnDislike.classList.remove('red');
-                    }
-                    btnLike.classList.add('green');
-                }
-                else if(d.statut == '1')
-                {
-                    if (btnLike.classList.contains('green')) {
-                        btnLike.classList.remove('green');
-                    }
-                    btnDislike.classList.add('red');
-                }
-                else if(d.statut == '0')
-                {
-                    if (btnLike.classList.contains('green')) {
-                        btnLike.classList.remove('green');
-                    }
-                    if (btnDislike.classList.contains('red')) {
-                        btnDislike.classList.remove('red');
-                    }
-                }
+                changebtnLike(d.idPost, d.statut);
             });
         },
     });
 }
 
-// if (btnLike !== null)
-// {
-//     btnLike.addEventListener('click', function() {
-//
-//         if (btnLike.classList.contains('red')) {
-//             btnDislike.classList.remove('red');
-//         }
-//         this.classList.toggle('green');
-//     });
-// }
-//
-// if (btnDislike !== null)
-// {
-//     btnDislike.addEventListener('click', function() {
-//
-//         if (btnLike.classList.contains('green')) {
-//             btnLike.classList.remove('green');
-//         }
-//         this.classList.toggle('red');
-//
-//     });
-// }
+function changebtnLike(idPost, statut)
+{
+    var btnLike = document.querySelector('#btnLike_' + idPost);
+    var btnDislike = document.querySelector('#btnDislike_' + idPost);
 
-
-
-
+    if(statut == '2')
+    {
+        if (btnDislike.classList.contains('red')) {
+            btnDislike.classList.remove('red');
+        }
+        btnLike.classList.add('green');
+    }
+    else if(statut == '1')
+    {
+        if (btnLike.classList.contains('green')) {
+            btnLike.classList.remove('green');
+        }
+        btnDislike.classList.add('red');
+    }
+    else if(statut == '0')
+    {
+        if (btnLike.classList.contains('green')) {
+            btnLike.classList.remove('green');
+        }
+        if (btnDislike.classList.contains('red')) {
+            btnDislike.classList.remove('red');
+        }
+    }
+}

fe0efb8148fc5769d15b0c27d2fe1aa106fb74ef
Zakaria <rhayatizakaria@gmail.com>
2022-02-07 09:19:51 +0100

    inbox/deleted/sent front

--- views/messagerie-inbox.php
+++ views/messagerie-inbox.php
@@ -2,6 +2,12 @@
 $page = 'inbox';
 require('shared/messagerie-header.php');
 ?>
-    content
+<div class="wrapper p-2">
+    <div class="row menu border-bottom pb-2">
+        <div class="col-1"><input type="checkbox" style="transform: scale(1.5);"></div>
+        <div class="col-1"><i class="fas fa-sync" style="font-size:24px"></i></div>
+        <div class="col-1"><i class="fas fa-trash-alt" style="font-size:24px"></i></div>
+    </div>
+</div>
 <?php include('shared/messagerie-footer.php');?>
 
